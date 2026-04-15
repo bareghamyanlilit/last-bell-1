34508c5941224cd885a380937ff46fb2ef9b113e
Lilit <baregamyanlilit36@gmail.com>
2026-04-15 04:41:01 +0400

    first commit

---
+++ components/Home.tsx
@@ -0,0 +1,169 @@
+"use client";
+import Image from "next/image";
+import { TimeBox } from "./TimeBox";
+import { useState } from "react";
+import { MusicPlayer } from "./music";
+import { anim, txt1, txt1_1, txt2, txt2_1, txt3 } from "@/data/data";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+export function Home() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  // const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2500);
+  // }, []);
+
+  // if (loading) {
+  //   return (
+  //     <div className="FontMassis flex items-center justify-center h-screen text-3xl">
+  //       Loading...
+  //     </div>
+  //   );
+  // }
+  return (
+    <div className="text-center tracking-[10%]  FontMassis overflow-hidden  FontMassis ">
+      {/* music button */}
+      <div>
+        <button
+          onClick={() => setIsPlaying(!isPlaying)}
+          className="fixed z-10 p-1  rounded-2xl bg-[#4a4a4a81]  right-7 top-7 w-15  h-15 flex justify-center items-center"
+        >
+          <Image
+            src="/icon1.png"
+            alt="icon1"
+            width={500}
+            height={500}
+            className=" w-full p-2 object-contain "
+          />
+        </button>
+
+        <MusicPlayer isPlaying={isPlaying} />
+      </div>
+
+      <div
+        className="  tracking-0 h-[90vh] bg-cover bg-top flex flex-col justify-between pt-20 p-7 "
+        style={{ backgroundImage: "url('/img1.jpg')" }}
+      >
+        <motion.h3
+          {...anim}
+          className="FontAdamathuz text-vrayi mt-13   relative z-0 text-[40px]"
+        >
+          Վերջին զանգ
+          <p className="FontAdamathuz absolute -z-1 text-8xl w-max top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-taki">
+            12 Ա
+          </p>
+        </motion.h3>
+        <TimeBox />
+      </div>
+
+      <div className=" px-5 my-30">
+        <motion.p
+          {...anim}
+          className="FontMassis text-vrayi relative z-0 text-3xl "
+        >
+          {txt1}
+          <span className="FontMassis absolute w-max -z-1 text-5xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  text-taki">
+            {txt1}
+          </span>
+        </motion.p>
+      </div>
+
+      <motion.div {...anim} className="px-5">
+        <Image
+          src="/img2.jpg"
+          alt="icon1"
+          width={500}
+          height={500}
+          className="w-full  object-cover rounded-xl mb-30 "
+        />
+      </motion.div>
+
+      <motion.h2
+        {...anim}
+        className="text-vrayi text-5xl my-30 text-center FontAdamathuz"
+      >
+        25/05/2026
+      </motion.h2>
+
+      <motion.p {...anim} className=" text-vrayi FontMassis text-3xl">
+        {txt2}
+      </motion.p>
+
+      <motion.div {...anim} className="px-5">
+        <Image
+          src="/icon1.png"
+          alt="icon1"
+          width={500}
+          height={500}
+          className=" w-32 mx-auto object-cover my-30"
+        />
+      </motion.div>
+
+      <motion.h3
+        {...anim}
+        className="FontMassis text-vrayi relative z-0 text-[40px]"
+      >
+        12 Տարի
+        <p className="FontAdamathuz tracking-[20%]  absolute -z-1 text-8xl w-max top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  text-taki">
+          12
+        </p>
+      </motion.h3>
+
+      <motion.h3
+        {...anim}
+        className="FontMassis my-30 text-vrayi relative z-0 text-[40px]"
+      >
+        626 Շաբաթ
+        <p className="FontAdamathuz tracking-[20%] absolute -z-1 text-8xl w-max top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  text-taki">
+          144
+        </p>
+      </motion.h3>
+
+      <motion.h3
+        {...anim}
+        className="FontMassis mb-30 text-vrayi relative z-0 text-[40px]"
+      >
+        144 Ամիս
+        <p className="FontAdamathuz tracking-[20%]  absolute -z-1 text-8xl w-max top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  text-taki">
+          626
+        </p>
+      </motion.h3>
+
+      <motion.h3 {...anim} className="text-vrayi FontMassis text-4xl">
+        ՄԻԼԻՈՆԱՎՈՐ ՀԻՇՈՂՈՒԹՅՈՒՆՆԵՐ
+      </motion.h3>
+
+      <Image
+        src="/img3.png"
+        alt="icon1"
+        width={500}
+        height={500}
+        className="w-full  object-cover my-20 "
+      />
+
+      <motion.p {...anim} className="my-30 text-vrayi FontMassis text-3xl">
+        {txt3}
+      </motion.p>
+
+      <Image
+        src="/img4.jpg"
+        alt="icon1"
+        width={500}
+        height={500}
+        className="w-full  object-cover my-20 "
+      />
+
+      <Image
+        src="/icon1.png"
+        alt="icon1"
+        width={500}
+        height={500}
+        className=" w-32 mx-auto object-cover my-20"
+      />
+    </div>
+  );
+}
---
+++ data/data.tsx
@@ -0,0 +1,38 @@
+export const program: any = [
+  {
+    time: "10:00",
+    title: "Փեսայի տուն",
+  },
+  {
+    time: "12:00",
+    title: "Հարսնացուի տուն",
+  },
+  {
+    time: "14:00",
+    title: "Ս․ Գայանե Եկեղեցի",
+  },
+  {
+    time: "17:00",
+    title: "Էլինար Ռեստորանային Համալիր",
+  },
+];
+export const txt1 =
+  "Սիրով\n հրավիրում ենք\n Ձեզ մեր\nՎԵՐՋԻՆ ԶԱՆԳԻ \nմիջոցառմանը";
+
+export const txt2 ="Օր երբ կփակվի մեր\nգրքից մի գլուխ,\nոր տարիներով\nգրում էինք \nՄԻԱՍԻՆ…\nհաջորդ օրվանից՝\nամեն մեկը սկսելու է \nիր լեգենդը";
+
+export const txt3 =
+  "Միջոցառումը\nՏեղի կունենա\nՄայիսի 25 ին\nԺամը 12:15 \nԵրևանի\nԹիվ 4 դպրոցի\nՄեծ դահլիճում\n\nՍիրով 12 Ա \nդասարան";
+
+export const anim: any = {
+  initial: { opacity: 0, y: 40 },
+  whileInView: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeIn",
+    },
+  },
+  viewport: { once: true },
+};
